0fe446a82e54d22c16ecfc9f05ba00b76f96cfb4
ravinasrivastava <ravinasribbu52@gmail.com>
2024-03-14 16:08:03 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LicenseAgreement from "./Policy/LicenseAgreement";
+import PrivacyPolicy from "./Policy/PrivacyPolicy";
+import Termsandconditions from "./Policy/Termsandconditions";
+function App() {
+  return (
+    <>
+    <Router>
+        <Routes>
+          <Route path="/LicenseAgreement" element={<LicenseAgreement />} />
+          {/* <Route path="*" element={<LicenseAgreement />} /> */}
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/Termsandconditions" element={<Termsandconditions />} />
+
+          <Route />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
