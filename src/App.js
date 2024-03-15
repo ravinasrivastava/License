49153175008caf8d6531d98741cc5b079e6e09ec
kuhukumawat <147390505+kuhukumawat@users.noreply.github.com>
2024-03-15 16:35:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LicenseAgreement from "./Policy/LicenseAgreement";
 import PrivacyPolicy from "./Policy/PrivacyPolicy";
 import Termsandconditions from "./Policy/Termsandconditions";
+import Form from './Policy/Form';
 function App() {
   return (
     <>
-    <Router>
+      <Router>
         <Routes>
           <Route path="/LicenseAgreement" element={<LicenseAgreement />} />
           <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
           <Route path="/Termsandconditions" element={<Termsandconditions />} />
-
-          <Route />
+          <Route path="/Form" element={<Form />} />
         </Routes>
       </Router>
     </>
