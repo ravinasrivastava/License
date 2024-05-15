cf45110f1dfe2317b73c9172caf89d3e552b8489
ravinasrivastava <90607740+ravinasrivastava@users.noreply.github.com>
2024-05-15 15:47:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LicenseAgreement from "./Components/Policy/LicenseAgreement";
 import PrivacyPolicy from "./Components/Policy/PrivacyPolicy";
 import Termsandconditions from "./Components/Policy/Termsandconditions";
-import PersonalData from "./Components/Policy/PersonalData";
-
+ 
 
 function App() {
   return (
@@ -14,8 +13,7 @@ function App() {
           <Route path="/" element={<LicenseAgreement />} />
           <Route exact path="/PrivacyPolicy" element={<PrivacyPolicy />} />
           <Route exact path="/Termsandconditions" element={<Termsandconditions />} />
-          <Route exact path="/PersonalData" element={<PersonalData />} />
-
+ 
 
           <Route />
         </Routes>
